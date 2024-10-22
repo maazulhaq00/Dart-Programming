813a30c02cd46deb941009f9459668c8fd1ed0bc
maazulhaq <maazulhaq00@gmail.com>
2024-10-22 12:28:09 +0500

    Typecasting

--- typeCasting.dart
+++ typeCasting.dart
@@ -1,25 +1,40 @@
-void typeCasting(){
-
+void typeCasting() {
+  // DOUBLE TO
   double percentage = 98.567;
-  int percentInt = percentage.toInt();
 
+  // double to int
+  int percentInt = percentage.toInt();
   print("percentage = $percentage; Type = ${percentage.runtimeType}");
   print("percentage = $percentInt; Type = ${percentInt.runtimeType}");
-
+  // double to str
   String percentStr = percentage.toString();
   print("percentage = $percentStr; Type = ${percentStr.runtimeType}");
 
+  // INTEGER TO
+  int age = 34;
+
+  // int to double
+  print("Age = ${age.toDouble()}, Type = ${age.toDouble().runtimeType}");
+  // int to str
+  print("Age = ${age.toString()}, Type = ${age.toString().runtimeType}");
+
+  // STR TO
+  // str to int (parse & tryParse)
+  String num1 = "45";
+  var num1Int = num.parse(num1); // OR int.parse(num1)
+  print("num1 = $num1Int; Type = ${num1Int.runtimeType}");
+
+  // str to double (parse & tryParse)
+  String num2 = "45.67";
+  var num2Double = num.parse(num2); // OR double.parse(num2)
+  print("num2 = $num2Double; Type = ${num2Double.runtimeType}");
+
+  // str to bool
+
+  // bool to int
+  // bool to double
+  // bool to str
   bool isStudent = true;
   String isStudentStr = isStudent.toString();
   print("is Student = $isStudentStr; Type = ${isStudentStr.runtimeType}");
-
-  String num1 = "45";
-  String num2 = "45.67";
-
-  var num1Int = num.parse(num1); // OR int.parse(num1)
-  var num2Double = num.parse(num2); // OR double.parse(num2)
-
-  print("num1 = $num1Int; Type = ${num1Int.runtimeType}"); 
-  print("num2 = $num2Double; Type = ${num2Double.runtimeType}");
-
 }
